64d88c7385476784678dc1e003621a407ae1e69e
hoon9405 <hkhxoxo@gmail.com>
2023-01-17 22:04:14 +0900

    preprocess runfile argument add

--- preprocess_codeemb_run.sh
+++ preprocess_codeemb_run.sh
@@ -1,112 +1,117 @@
-# #whole text
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr mimiciii \
-# --first_icu \
-# --data /home/data_storage/mimiciii-1.4/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-#  --emb_type textbase --feature whole &&
+dest=$1
+data=$2
 
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr eicu \
-# --first_icu \
-# --data /home/data_storage/eicu-2.0/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-#  --emb_type textbase --feature whole &&
-
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr mimiciv \
-# --first_icu \
-# --data /home/data_storage/MIMIC-IV-2.0/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-#  --emb_type textbase --feature whole &&
-
-#whole code
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr mimiciii \
-# --first_icu \
-# --data /home/data_storage/mimiciii-1.4/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-#  --cache --emb_type codebase --feature whole &&
-
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr eicu \
-# --first_icu \
-# --data /home/data_storage/eicu-2.0/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-#  --emb_type codebase --feature whole &&
-
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr mimiciv \
-# --first_icu \
-# --data /home/data_storage/MIMIC-IV-2.0/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-#  --emb_type codebase --feature whole &&
-
-
-# #select text
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr mimiciii \
-# --first_icu \
-# --data /home/data_storage/mimiciii-1.4/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-#  --emb_type textbase --feature "select" &&
-
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr eicu \
-# --first_icu \
-# --data /home/data_storage/eicu-2.0/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-#  --emb_type textbase --feature "select" &&
-
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr mimiciv \
-# --first_icu \
-# --data /home/data_storage/MIMIC-IV-2.0/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-#  --emb_type textbase --feature "select" &&
-
-#select code
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr mimiciii \
-# --first_icu \
-# --data /home/data_storage/mimiciii-1.4/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-#  --emb_type codebase --feature "select" &&
+echo "dest" $dest
+echo "data" $data
+#whole text
+python3 main.py \
+--dest $dest \
+--ehr mimiciii \
+--first_icu \
+--data $2/mimiciii-1.4/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+ --emb_type textbase --feature "whole" &&
 
 python3 main.py \
---dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
+--dest $dest \
 --ehr eicu \
 --first_icu \
---data /home/data_storage/eicu-2.0/ \
+--data $2/eicu-2.0/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+ --emb_type textbase --feature "whole" &&
+
+python3 main.py \
+--dest $dest \
+--ehr mimiciv \
+--first_icu \
+--data $2/MIMIC-IV-2.0/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+ --emb_type textbase --feature "whole" &&
+
+#whole code
+python3 main.py \
+--dest $dest \
+--ehr mimiciii \
+--first_icu \
+--data $2/mimiciii-1.4/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+ --cache --emb_type codebase --feature "whole" &&
+
+python3 main.py \
+--dest $dest \
+--ehr eicu \
+--first_icu \
+--data $2/eicu-2.0/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+ --emb_type codebase --feature "whole" &&
+
+python3 main.py \
+--dest $dest \
+--ehr mimiciv \
+--first_icu \
+--data $2/MIMIC-IV-2.0/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+ --emb_type codebase --feature "whole" &&
+
+
+#select text
+python3 main.py \
+--dest $dest \
+--ehr mimiciii \
+--first_icu \
+--data $2/mimiciii-1.4/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+ --emb_type textbase --feature "select" &&
+
+python3 main.py \
+--dest $dest \
+--ehr eicu \
+--first_icu \
+--data $2/eicu-2.0/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+ --emb_type textbase --feature "select" &&
+
+python3 main.py \
+--dest $dest \
+--ehr mimiciv \
+--first_icu \
+--data $2/MIMIC-IV-2.0/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+ --emb_type textbase --feature "select" &&
+
+#select code
+python3 main.py \
+--dest $dest \
+--ehr mimiciii \
+--first_icu \
+--data $2/mimiciii-1.4/ \
 --readmission --mortality --los_3day --los_7day --long_term_mortality \
 --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
  --emb_type codebase --feature "select" &&
 
 python3 main.py \
---dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
+--dest $dest \
+--ehr eicu \
+--first_icu \
+--data $2/eicu-2.0/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+ --emb_type codebase --feature "select" &&
+
+python3 main.py \
+--dest $dest \
 --ehr mimiciv \
 --first_icu \
---data /home/data_storage/MIMIC-IV-2.0/ \
+--data $2/MIMIC-IV-2.0/ \
 --readmission --mortality --los_3day --los_7day --long_term_mortality \
 --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
  --emb_type codebase --feature "select" 
--- preprocess_run.sh
+++ preprocess_run.sh
@@ -1,112 +1,118 @@
-# #whole text
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr mimiciii \
-# --first_icu \
-# --data /home/data_storage/mimiciii-1.4/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-# --cache --emb_type textbase --feature whole &&
+dest=$1
+data=$2
+
+echo "dest" $dest
+echo "data" $data
+
+#"whole" text
+python3 main.py \
+--dest $dest \
+--ehr mimiciii \
+--first_icu \
+--data $data/mimiciii-1.4/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+--cache --emb_type textbase --feature "whole" &&
 
 python3 main.py \
---dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
+--dest $dest \
 --ehr eicu \
 --first_icu \
---data /home/data_storage/eicu-2.0/ \
+--data $data/eicu-2.0/ \
 --readmission --mortality --los_3day --los_7day --long_term_mortality \
 --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
---cache --emb_type textbase --feature whole &&
+--cache --emb_type textbase --feature "whole" &&
 
 python3 main.py \
---dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
+--dest $dest \
 --ehr mimiciv \
 --first_icu \
---data /home/data_storage/MIMIC-IV-2.0/ \
+--data $data/MIMIC-IV-2.0/ \
 --readmission --mortality --los_3day --los_7day --long_term_mortality \
 --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
---cache --emb_type textbase --feature whole &&
+--cache --emb_type textbase --feature "whole" &&
 
-# #whole code
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr mimiciii \
-# --first_icu \
-# --data /home/data_storage/mimiciii-1.4/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-# --cache --emb_type codebase --feature whole &&
+#""whole"" code
+python3 main.py \
+--dest $dest \
+--ehr mimiciii \
+--first_icu \
+--data $data/mimiciii-1.4/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+--cache --emb_type codebase --feature "whole" &&
 
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr eicu \
-# --first_icu \
-# --data /home/data_storage/eicu-2.0/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-# --cache --emb_type codebase --feature whole &&
+python3 main.py \
+--dest $dest \
+--ehr eicu \
+--first_icu \
+--data $data/eicu-2.0/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+--cache --emb_type codebase --feature "whole" &&
 
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr mimiciv \
-# --first_icu \
-# --data /home/data_storage/MIMIC-IV-2.0/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-# --cache --emb_type codebase --feature whole &&
+python3 main.py \
+--dest $dest \
+--ehr mimiciv \
+--first_icu \
+--data $data/MIMIC-IV-2.0/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+--cache --emb_type codebase --feature "whole" &&
 
 
 #select text
 python3 main.py \
---dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
+--dest $dest \
 --ehr mimiciii \
 --first_icu \
---data /home/data_storage/mimiciii-1.4/ \
+--data $data/mimiciii-1.4/ \
 --readmission --mortality --los_3day --los_7day --long_term_mortality \
 --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
 --cache --emb_type textbase --feature "select" &&
 
 python3 main.py \
---dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
+--dest $dest \
 --ehr eicu \
 --first_icu \
---data /home/data_storage/eicu-2.0/ \
+--data $data/eicu-2.0/ \
 --readmission --mortality --los_3day --los_7day --long_term_mortality \
 --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
 --cache --emb_type textbase --feature "select" &&
 
 python3 main.py \
---dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
+--dest $dest \
 --ehr mimiciv \
 --first_icu \
---data /home/data_storage/MIMIC-IV-2.0/ \
+--data $data/MIMIC-IV-2.0/ \
 --readmission --mortality --los_3day --los_7day --long_term_mortality \
 --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
 --cache --emb_type textbase --feature "select" 
 
-# #select code
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr mimiciii \
-# --first_icu \
-# --data /home/data_storage/mimiciii-1.4/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-# --cache --emb_type codebase --feature "select" &&
+#select code
+python3 main.py \
+--dest $dest \
+--ehr mimiciii \
+--first_icu \
+--data $data/mimiciii-1.4/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+--cache --emb_type codebase --feature "select" &&
 
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr eicu \
-# --first_icu \
-# --data /home/data_storage/eicu-2.0/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-# --cache --emb_type codebase --feature "select" &&
+python3 main.py \
+--dest $dest \
+--ehr eicu \
+--first_icu \
+--data $data/eicu-2.0/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+--cache --emb_type codebase --feature "select" &&
 
-# python3 main.py \
-# --dest /nfs_edlab/ghhur/UniHPF/input_test2/ \
-# --ehr mimiciv \
-# --first_icu \
-# --data /home/data_storage/MIMIC-IV-2.0/ \
-# --readmission --mortality --los_3day --los_7day --long_term_mortality \
-# --final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
-# --cache --emb_type codebase --feature "select" &&
+python3 main.py \
+--dest $dest \
+--ehr mimiciv \
+--first_icu \
+--data $data/MIMIC-IV-2.0/ \
+--readmission --mortality --los_3day --los_7day --long_term_mortality \
+--final_acuity --imminent_discharge --diagnosis --creatinine --bilirubin --platelets --wbc \
+--cache --emb_type codebase --feature "select" &&
